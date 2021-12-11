a554599e8e28bfe8ecece5affcfc4cea29f9fd41
YakovchukDev <yakovchukdev@gmail.com>
2021-12-11 21:55:13 +0200

    Delete trash

--- Assets/Scripts/IntentoryControl.cs
+++ Assets/Scripts/IntentoryControl.cs
@@ -7,17 +7,8 @@ public class IntentoryControl : MonoBehaviour
 {
     [SerializeField]
     private Image[] InventoryItems;
-    public Sprite[] Items;
     private int count = 0;
 
-    void Start()
-    {
-        foreach(var obj in Items)
-        AddItemInInventory(obj);
-        
-        for(int i = 0; i < 6; i++)
-        RemoveItemInInventory(i);
-    }
     public void AddItemInInventory(Sprite _sprite)
     {
         InventoryItems[count].sprite = _sprite;
