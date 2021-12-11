31d0389470e00f8275d84ea25c2ccbd3d98a61e1
maRina <73962486+Rina-redis@users.noreply.github.com>
2021-12-11 15:54:39 +0200

    Can Interacct with object

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float SpeedHorizontal;
 
+    private Camera mainCamera;
+
+
+    [SerializeField]
+    private LayerMask demention1;
+    private LayerMask demention2;
+
     private Rigidbody2D _playerRb;
     private Transform _playerTransform;
 
     void Start()
     {
+        mainCamera = Camera.main;
         _playerRb = GetComponent<Rigidbody2D>();
         _playerTransform = GetComponent<Transform>();
     }
@@ -22,6 +30,21 @@ public class PlayerMovement : MonoBehaviour
         if(HorizontalMovement != 0)
         {
             _playerRb.AddForce(_playerTransform.right * SpeedHorizontal * HorizontalMovement);
+        }
+
+        if (Input.GetButtonDown("Fire1") )
+        {
+            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            var collider = Physics2D.OverlapPoint(mousePos, demention1);
+            if(collider != null)
+            {
+                InteractableObject interactable = collider.GetComponent<InteractableObject>();
+                if(interactable != null)
+                {
+                    interactable.Interact();
+                    Debug.Log(interactable?.name);
+                }
+            }
         }
     }
 }
