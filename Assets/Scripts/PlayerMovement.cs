0ade26df33468566b5e4aa84a3f58f9c088ff99f
YakovchukDev <yakovchukdev@gmail.com>
2021-12-12 01:04:42 +0200

    Set up animator and conect animation to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float SpeedHorizontal;
 
+    private Animator _animation;
     private Rigidbody2D _playerRb;
     private Transform _playerTransform;
+    private SpriteRenderer _spriteRenderer;
 
     void Start()
     {  
         _playerRb = GetComponent<Rigidbody2D>();
         _playerTransform = GetComponent<Transform>();
+        _animation = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -21,7 +25,20 @@ public class PlayerMovement : MonoBehaviour
         float HorizontalMovement = Input.GetAxis("Horizontal");   
         if(HorizontalMovement != 0)
         {
+            if(HorizontalMovement < 0)
+            {
+                _spriteRenderer.flipX = true;
+            }
+            else
+            {
+                _spriteRenderer.flipX = false;
+            }
+            _animation.SetBool("IsRun", true);
             _playerRb.AddForce(_playerTransform.right * SpeedHorizontal * HorizontalMovement);
+        }
+        else
+        {
+            _animation.SetBool("IsRun", false);
         }
      
     }
